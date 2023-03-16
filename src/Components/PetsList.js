0ca697d15433a1182ba18fadfda8bd2ec91e3d63
ReemAlchal <reemalchal@hotmail.com>
2023-03-16 21:03:07 +0300

    onchange-onclick-usestate

--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,9 +1,26 @@
 import pets from "../petsData";
 import PetItem from "./PetItem";
+import { useState } from "react";
+import App from "../App";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
-
+  const [query, setQuery] = useState("");
+  const [type, settype] = useState("");
+  const petList = pets
+    .filter((pet) => pet.name.toLowerCase().includes(query))
+    .filter((pet) => {
+      if (type == "") {
+        return true;
+      }
+      return pet.type == type;
+    })
+    .map((pet) => <PetItem pet={pet} key={pet.id} />);
+  function SearchBar(x) {
+    setQuery(x.target.value);
+  }
+  function PetSelector(x) {
+    settype(x.target.value);
+  }
   return (
     <section id="doctors" className="doctor-section pt-140">
       <div className="container">
@@ -20,11 +37,17 @@ function PetsList() {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  onChange={SearchBar}
+                  value={query}
                 />
               </div>
               <br />
               Type:
-              <select className="form-select">
+              <select
+                onChange={PetSelector}
+                className="form-select"
+                value={type}
+              >
                 <option value="" selected>
                   All
                 </option>
